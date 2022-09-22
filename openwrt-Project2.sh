b25a563236ad809bd6556291fc8681db27fa7d91
afala2020 <71696868+afala2020@users.noreply.github.com>
2022-09-22 21:22:09 +0800

    Update openwrt-Project2.sh

--- openwrt-Project2.sh
+++ openwrt-Project2.sh
@@ -20,14 +20,14 @@
 # 管理地址
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
-# 默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
-
-# 修改NAS为网络存储
-sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
-
 # 修改固件默认时区
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
 # 修改固件默认名称
 sed -i 's/ImmortalWrt/HIWIFI/g' package/base-files/files/bin/config_generate
+
+# 默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+
+# 修改NAS为网络存储
+sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
